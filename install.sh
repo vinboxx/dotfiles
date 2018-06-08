c87025c8b9df43e0a3e17a718f3fb01730064eb1
Weerapat Poosri <vinboxx@gmail.com>
2018-06-08 10:09:09 +0700

    Add iterm2 config and setup file

--- install.sh
+++ install.sh
@@ -1,1 +1,16 @@
-cp -r $HOME/dotfiles/.bash_aliases $HOME/dotfiles/.bash_profile $HOME/dotfiles/.zshrc $HOME/.
+create_or_replace_symlink() {
+    local source=$1
+    local destination=$2
+
+    if [ -L $destination ]; then
+        rm $destination
+    fi
+
+    ln -nfs $source $destination
+}
+
+create_or_replace_symlink $HOME/dotfiles/.bash_aliases $HOME/.bash_aliases
+create_or_replace_symlink $HOME/dotfiles/.bash_profile $HOME/.bash_profile
+create_or_replace_symlink $HOME/dotfiles/.zshrc $HOME/.zshrc
+
+$HOME/dotfiles/scripts/setup.sh
