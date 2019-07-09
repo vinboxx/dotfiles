5900c226c064c950e25aaae922db5f42d35ce7ed
Weerapat Poosri <vinboxx@gmail.com>
2019-07-09 14:07:50 +0700

    Add git-author-rewrite.sh

---
+++ scripts/git-author-rewrite.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+git filter-branch -f --env-filter '
+
+OLD_NAME="Old name"
+OLD_EMAIL="old@example.com"
+CORRECT_NAME="New name"
+CORRECT_EMAIL="new@example.com"
+
+if [ "$GIT_COMMITTER_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_COMMITTER_NAME="$CORRECT_NAME"
+    export GIT_COMMITTER_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_EMAIL" = "$OLD_EMAIL" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+if [ "$GIT_AUTHOR_NAME" = "$OLD_NAME" ]
+then
+    export GIT_AUTHOR_NAME="$CORRECT_NAME"
+    export GIT_AUTHOR_EMAIL="$CORRECT_EMAIL"
+fi
+' --tag-name-filter cat -- --branches --tags
+
+git config user.name "$CORRECT_NAME"
+git config user.email "$CORRECT_EMAIL"
