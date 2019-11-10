3b1d3b60cbc9c72ca0e47c34cb39fa04d2372bd2
Weerapat Poosri <vinboxx@gmail.com>
2019-11-10 23:46:57 +0700

    Update zsh plugin script

--- scripts/setup/linux/zsh.sh
+++ scripts/setup/linux/zsh.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
+function clone_plugin {
+  if [ ! -d "$2" ] ; then
+    echo "Installing: ${1}..."
+    git clone "$1" "$2"
+  else
+    echo "Already installed: ${1}"
+  fi
+}
+
+clone_plugin https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+clone_plugin https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+clone_plugin https://github.com/agkozak/zsh-z ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-z
