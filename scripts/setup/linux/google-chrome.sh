bc88caa1123fc716e7e4508d7d65ec12cb81cd72
Weerapat Poosri <vinboxx@gmail.com>
2019-11-10 23:29:53 +0700

    Update google chrome script

--- scripts/setup/linux/google-chrome.sh
+++ scripts/setup/linux/google-chrome.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 
-wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+if [ `which google-chrome-stable` ]; then
+  echo "Already installed: google-chrome-stable"
+else
+  echo "Installing: google-chrome-stable..."
+  wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
 
-sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+  sudo sh -c 'echo "deb [arch=amd64] http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
 
-sudo apt update
-sudo apt install google-chrome-stable
+  sudo apt update
+  sudo apt install google-chrome-stable
+fi
