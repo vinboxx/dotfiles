7dfe31af96d47e759976f2173cb5dcf9c64af3c0
Weerapat Poosri <vinboxx@gmail.com>
2020-09-02 09:04:09 +0700

    Update homebrew script to check existing app first

--- scripts/setup/02-homebrew.sh
+++ scripts/setup/02-homebrew.sh
@@ -1,81 +1,135 @@
-#!/usr/bin/env bash
+#!/bin/bash
 
 source "$HOME/dotfiles/scripts/setup/utils.sh"
 
 echo "=> Homebrew"
 
-if [[ ! -f $(which brew) ]]
-then
-  print_info "Installing..."
+# Homebrew Formulae
+# https://github.com/Homebrew/homebrew
 
-  /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
-  ROOT_DIR="$(cd "$(dirname "$0")"; pwd -P)"
-  brew bundle --file="$ROOT_DIR/core/Brewfile"
-  brew cleanup && brew upgrade && brew update && brew doctor
-
-  print_success "Completed..."
-else
-  print_success "Skipping..."
-fi
-
-###############################################################################
-# Brew install                                                                #
-###############################################################################
-brew install python
-brew install git
-brew install zsh zsh-completions zsh-autosuggestions zsh-syntax-highlighting
-brew install mas
-brew install nvm
-mkdir ~/.nvm
-
-###############################################################################
-# Brew cask                                                                   #
-###############################################################################
-# to maintain cask ....
-#     brew update && brew upgrade brew-cask && brew cleanup && brew cask cleanup`
-
-# Dev
-brew cask install cyberduck
-brew cask install docker
-brew cask install iterm2
-brew cask install postman
-brew cask install tableplus
-brew cask install visual-studio-code
-
-# Browsers
-brew cask install firefox
-brew cask install google-chrome
-
-# Media
-brew cask install spotify
-
-# Utilities
-brew cask install alfred
-brew cask install bettertouchtool
-brew cask install dropbox
-brew cask install istat-menus
-brew cask install notion
-brew cask install snagit
-brew cask install the-unarchiver
-brew cask install tunnelblick
-brew cask install WebPQuickLook
-
-# Chat
-brew cask install google-chat
-brew cask install slack
-
-# Fonts
-brew tap homebrew/cask-fonts
-brew cask install font-fira-code
-brew cask install font-source-code-pro
-brew cask install font-source-code-pro-for-powerline
-
-declare -a mas_apps=(
-  '1295203466'  # Microsoft Remote Desktop 10
-  '539883307'   # LINE
-  '824183456'   # Affinity Photo
+declare -r -a HOMEBREW_FORMULAE=(
+    "python"
+    "zsh-completions"
+    "zsh-autosuggestions"
+    "zsh-syntax-highlighting"
+    "git"
+    "mas"
+    "nvm"
 )
 
-for app in "${mas_apps[@]}"; do
-  mas install "$app"
-done
+# Homebrew Cask
+# https://github.com/caskroom/homebrew-cask
+
+declare -r -a HOMEBREW_CASKS=(
+    # Dev
+    "cyberduck"
+    "docker"
+    "iterm2"
+    "postman"
+    "tableplus"
+    "visual-studio-code"
+
+    # Browsers
+    "firefox"
+    "google-chrome"
+
+    # Media
+    "spotify"
+
+    # Utilities
+    "alfred"
+    "bettertouchtool"
+    "dropbox"
+    "istat-menus"
+    "notion"
+    "snagit"
+    "the-unarchiver"
+    "tunnelblick"
+    "WebPQuickLook"
+
+    # Chat
+    "google-chat"
+    "slack"
+)
+
+# Homebrew Cask Fonts
+# https://github.com/Homebrew/homebrew-cask-fonts
+
+declare -r -a HOMEBREW_CASK_FONTS=(
+    "font-fira-code"
+    "font-source-code-pro"
+    "font-source-code-pro-for-powerline"
+)
+
+declare -a mas_apps=(
+    '1295203466'  # Microsoft Remote Desktop 10
+    '539883307'   # LINE
+    '824183456'   # Affinity Photo
+)
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+brew_install() {
+
+    declare -r -a FORMULAE=("${!1}"); shift;
+    declare -r CMD="$1"
+
+    for i in ${!FORMULAE[*]}; do
+        tmp="${FORMULAE[$i]}"
+        [ $(brew "$CMD" list "$tmp" &> /dev/null; printf $?) -eq 0 ] \
+            && print_success "$tmp" \
+            || execute "brew $CMD install $tmp" "$tmp"
+    done
+
+}
+
+brew_tap() {
+
+    declare -r REPOSITORY="$1"
+
+    brew tap "$REPOSITORY" &> /dev/null
+
+    [ "$(brew tap | grep "$REPOSITORY" &> /dev/null; printf $?)" -eq 0 ] \
+        && (print_success "brew tap ($REPOSITORY)"; return 0) \
+        || (print_error "brew tap ($REPOSITORY)"; return 1)
+
+}
+
+# - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+
+main() {
+
+    local i="", tmp=""
+
+    # Homebrew
+    if [ $(cmd_exists "brew") -eq 1 ]; then
+        printf "\n" | /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install.sh)"
+        #  └─ simulate the ENTER keypress
+        print_result $? "brew"
+    fi
+
+    if [ $(cmd_exists "brew") -eq 0 ]; then
+
+        execute "brew update" "brew (update)"
+        execute "brew upgrade --all" "brew (upgrade)"
+        execute "brew cleanup" "brew (cleanup)"
+        printf "\n"
+
+        brew_install "HOMEBREW_FORMULAE[@]"
+        printf "\n"
+
+        brew_install "HOMEBREW_CASKS[@]" "cask"
+        printf "\n"
+
+        brew_tap "homebrew/cask-fonts" \
+            && brew_install "HOMEBREW_CASK_FONTS[@]" "cask"
+        printf "\n"
+
+        for app in "${mas_apps[@]}"; do
+            mas install "$app"
+        done
+
+    fi
+}
+
+main
--- scripts/setup/utils.sh
+++ scripts/setup/utils.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+cmd_exists() {
+    [ -x "$(command -v "$1")" ] \
+        && printf 0 \
+        || printf 1
+}
+
 create_or_replace_symlink() {
     local source=$1
     local destination=$2
@@ -11,9 +17,28 @@ create_or_replace_symlink() {
     ln -nfs $source $destination
 }
 
+execute() {
+    $1 &> /dev/null
+    print_result $? "${2:-$1}"
+}
+
+print_error() {
+    # Print output in red
+    printf "\e[0;31m  [✖] $1 $2\e[0m\n"
+}
+
 print_info() {
     # Print output in purple
     printf "\n\e[0;35m $1\e[0m\n\n"
+}
+
+print_result() {
+    [ $1 -eq 0 ] \
+        && print_success "$2" \
+        || print_error "$2"
+
+    [ "$3" == "true" ] && [ $1 -ne 0 ] \
+        && exit
 }
 
 print_success() {
